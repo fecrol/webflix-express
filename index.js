69346729699ed3d99e85cefd2ca40e35c1622597
Maciej Fec <maciejfec1996@gmail.com>
2023-01-29 19:04:17 +0000

    dummy code

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 if(process.env.NODE_ENV !== "production") require("dotenv").config({ path: "./.env" })
-const app = express()
-const PORT = process.env.PORT
 const loginRoute = require("./routes/login")
 const registerRoute = require("./routes/register")
 const moviesRoute = require("./routes/movies")
+const db = require("./db/MySqlDb")
+const User = require("./models/User")
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + "/public"))
 
 app.set("view engine", "ejs")
 
@@ -12,9 +19,16 @@ app.use("/login", loginRoute)
 app.use("/register", registerRoute)
 app.use("/movies", moviesRoute)
 
-app.get("/", (req, res) => {
-    res.status(200)
-    res.render("home")
+app.get("/", async (req, res) => {
+    const dbConn = db.connect()
+
+    const user = new User({dbConn})
+    let results = await user.readAll()
+    results = results.length === 0 ? {message: "no results found"} : results
+
+    dbConn.end()
+
+    results.error ? res.status(500).json(results) : res.status(200).json(results)
 })
 
 app.listen(PORT, () => {
