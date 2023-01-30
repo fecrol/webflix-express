73eaa16c97bcfd4bdfa4c900df780c877a662c32
Maciej Fec <maciejfec1996@gmail.com>
2023-01-30 21:34:55 +0000

    add validation for empty fields

--- routes/register.js
+++ routes/register.js
@@ -23,12 +23,16 @@ route.post("/", async (req, res) => {
     const password = req.body.password
     const repeatPassword = req.body.repeatPassword
 
+    if(firstName.length === 0) return res.redirect("/register?error=first%20name%20is%20required")
+    if(lastName.length === 0) return res.redirect("/register?error=last%20name%20is%20required")
+    if(email.length === 0) return res.redirect("/register?error=email%20is%20required")
+    if(password.length === 0) return res.redirect("/register?error=password%20is%20required")
+    if(repeatPassword !== password) return res.redirect("/register?error=passwords%20do%20not%20match")
+
     const user = new User({dbConn, id, firstName, lastName, email, password})
     
     let result = await user.readByEmail(email)
     if(result && !result.message) return res.redirect("/register?error=user%20already%20exists")
-
-    if(repeatPassword !== password) return res.redirect("/register?error=passwords%20do%20not%20match")
 
     result = await user.create()
 
